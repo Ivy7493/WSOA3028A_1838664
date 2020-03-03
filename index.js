d206b1a725247e2c2b6c234133aee934a3e76ad2
Ivy7493 <60912637+Ivy7493@users.noreply.github.com>
2020-03-03 09:57:49 +0200

    trying to fix double button

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 
 
-//let BlogButton = document.createElement("button");
-//BlogButton.innerHTML = "Blog";
-//find the button by class name
-//let x = document.getElementsByClassName("Navigation")[0];
-let y = document.getElementsByClassName("Y")[0];
-//this is how you append a button
-//x.appendChild(BlogButton);
 
+let y = document.getElementsByClassName("Y")[0];
 y.addEventListener("click",function(){
 
     location.href = "blog.html";
     alert("Ivan!");
 });
-
-//BlogButton.addEventListener("click", function(){
-    
-  //  location.href = "blog.html";
-    //alert("U R GOING TO BLOG!");
-    //});
