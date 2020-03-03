1995341be14a34003910d8de048856a43c01e079
Ivy7493 <60912637+Ivy7493@users.noreply.github.com>
2020-03-03 10:00:08 +0200

    attempt 2 at fixing button

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 let y = document.getElementsByClassName("Y")[0];
 y.addEventListener("click",function(){
 
-    location.href = "blog.html";
+    location.href = newFunction();
     alert("Ivan!");
 });
+function newFunction() {
+    return "blog.html";
+}
+
