a0cbd111545623158ae0564ce5da3d39d6054228
Ivy7493 <60912637+Ivy7493@users.noreply.github.com>
2020-03-05 10:05:41 +0200

    Added contact page
    - added blog post
    - added contact page
    - added contact button

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 
 
 
-let y = document.getElementsByClassName("Y")[0];
+let Development_Button = document.getElementsByClassName("Development_Button")[0];
+let Contact_Button = document.getElementsByClassName("Contact_Button")[0];
 
-y.addEventListener("click",function(){
+Contact_Button.addEventListener("click",function(){
+
+    location.href = "contact.html";
+    alert("Contact page");
+
+})
+
+Development_Button.addEventListener("click",function(){
 
     location.href = "blog.html";
-    alert("Ivan!");
+    alert("Blog page");
 });
 
