27ffa5d8699f2fca030f7d132cda8307ddb77173
Ivy7493 <60912637+Ivy7493@users.noreply.github.com>
2020-03-10 08:02:52 +0200

    fuck it we doing it live

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 
-
-
 let Development_Button = document.getElementsByClassName("Development_Button")[0];
+Development_Button.addEventListener("click",function(){location.href = "blog.html";});
+
+let x = document.getElementsByClassName("Test")[0];
+x.addEventListener("click",function(){location.href = "contact.html";});
+
 let Contact_Button = document.getElementsByClassName("Contact_Button")[0];
-
-Contact_Button.addEventListener("click",function(){
-
-    location.href = "contact.html";
-    alert("Contact page");
-
-});
-
-Development_Button.addEventListener("click",function(){
-
-   location.href = "blog.html";
-    alert("Blog page");
-});
+Contact_Button.addEventListener("click",function(){location.href = "contact.html";});
 
