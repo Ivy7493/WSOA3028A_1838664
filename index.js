437072d5bb3b10f42c24c68c88bc428c1603f50d
Ivy7493 <60912637+Ivy7493@users.noreply.github.com>
2020-04-21 09:29:48 +0200

    Moved Social Media Sureillance

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ let Development_Button = document.querySelector('#Development_Button');
 Development_Button.addEventListener('click', ()=> location.href = "BlogIndex.html");
 let Home_Button = document.querySelector("#Home_Button");
 Home_Button.addEventListener("click",()=> location.href = "index.html");
+let Social_Button = document.querySelector('#Social_Button')
+Social_Button.addEventListener("click",()=> location.href = "Social/Social.html");
  
 
